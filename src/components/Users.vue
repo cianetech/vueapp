562e032fe37d2aae7d6b5e69d7636b52b4f69cb4
Graciane Nobre <cianemibs@hotmail.com>
2020-01-14 15:07:53 -0300

    Initial commit

---
+++ src/components/Users.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Users:</h3>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Users'
+}
+</script>
+
+<style>
+    h3 {
+        margin-bottom: 5%;
+    }
+</style>
