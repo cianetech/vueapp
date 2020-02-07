5adcd5771d2e14cd081591c5b98c962ac9a055e8
Ciane <cianemibs@hotmail.com>
2020-02-07 18:03:59 -0300

    adding bootstrap

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,12 +1,38 @@
 <template>
-    <h3>Users:</h3>
+    <div class="container">
+        <h3>Users:</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>City</th>
+                </tr>
+
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 import axios from 'axios';
 
 export default {
-    name: 'Users'
+    name: 'Users',
+    data() {
+        return {
+            users: null
+        };
+    },
+    created: function() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                this.users = res.data;
+            })
+    }
 }
 </script>
 
